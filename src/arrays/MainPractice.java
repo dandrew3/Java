32094773ae956f7bab16f11ebed7340507c96f0b
dandrew3 <80789229+dandrew3@users.noreply.github.com>
2021-12-17 20:46:25 -0600

    Update MainPractice.java

--- src/arrays/MainPractice.java
+++ src/arrays/MainPractice.java
@@ -6,6 +6,7 @@ public class MainPractice {
     public static void main(String[] args) {
         String[] str = new String[4];
         str[0] = "Hi";
+        str[1] = "Andrew";
         str[2] = "Vasulko";
         System.out.println(Arrays.toString(str));
     }
